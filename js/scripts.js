319a73a67722ded3df7b52811b425b5bdcf112fb
Tori FavWest <practiceandthenpractice@gmail.com>
2021-03-26 10:43:40 -0700

    make scores for first two questions appear correctly

--- js/scripts.js
+++ js/scripts.js
@@ -14,15 +14,30 @@ function calculateResults(answer1, answer2) {
   else if (answer1==="disagree") {
     pythonScore -= 5
   }
-  else if (answer1==="nope") {
+  else if (answer1==="strongly-disagree") {
     pythonScore -= 100;
   }
 
   //SCORE STRONGLY_TYPED QUESTION
   if (answer2==="strongly-agree") {
+    jsScore -=100;
     pythonScore -=100;
     javaScore +=10;
     kotlinScore +=10;
+  }
+  else if (answer2==="agree") {
+    jsScore -=5;
+    pythonScore -=5;
+    javaScore +=5;
+    kotlinScore +=5;
+  }
+  else if (answer2==="disagree") {
+    javaScore -=5;
+    kotlinScore -=5;
+  }
+  else if (answer2==="strongly-disagree") {
+    javaScore -=100;
+    kotlinScore -=100;
   }
 
   //RETURN RESULTS
@@ -37,7 +52,8 @@ function calculateResults(answer1, answer2) {
 $(document).ready(function() {
   $("#all-questions").submit(function(event) {
     event.preventDefault();
-    const rec1= calculateResults($("input:radio[name=whitespace]:checked").val());
+    const rec1= calculateResults($("input:radio[name=whitespace]:checked").val(),
+    $("input:radio[name=strongly-typed]:checked").val());
     $("#rec1").text(rec1);
     $("#recommendation1").show();
   });
