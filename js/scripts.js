c3455e822d2f995aa7aef6644eebe292ef0d1ba1
Tori FavWest <practiceandthenpractice@gmail.com>
2021-03-26 10:22:43 -0700

    add script to track score for each language

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,40 @@
-function whitespace(answer) {
-  if (answer==="strongly-agree") {
-    return "Python";
+function calculateResults(answer1) {
+  let pythonScore=0;
+  let javaScore=0;
+  let jsScore=0;
+  let kotlinScore=0;
+
+  pythonScore=whitespace(answer1, pythonScore);
+  
+  if (pythonScore > 0) {
+    return "You might like Python";
   }
-  else if (answer==="agree") {
-    return "Python";
+  else {
+    return "Sorry, something went wrong"
   }
-  else if (answer==="disagree") {
-    return "not Python";
+}
+
+function whitespace(answer1, pythonScore) {
+  if (answer1==="strongly-agree") {
+    pythonScore += 10;
   }
-  else if (answer==="nope") {
-    return "not Python";
+  else if (answer1==="agree") {
+    pythonScore += 5;
   }
+  else if (answer1==="disagree") {
+    pythonScore -= 5
+  }
+  else if (answer1==="nope") {
+    pythonScore -= 100;
+  }
+  return pythonScore;
 }
 
 $(document).ready(function() {
   $("#all-questions").submit(function(event) {
     event.preventDefault();
-    const answer = whitespace($("input:radio[name=whitespace]:checked").val());
-    $("#rec1").text(answer);
+    const rec1= calculateResults($("input:radio[name=whitespace]:checked").val());
+    $("#rec1").text(rec1);
     $("#recommendation1").show();
   });
 });
