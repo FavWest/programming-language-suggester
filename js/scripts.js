ca0c924c2da32a042374259d315f854b7326253b
Tori FavWest <practiceandthenpractice@gmail.com>
2021-03-26 09:29:29 -0700

    change recommendation based on user input

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,23 @@
+function whitespace(answer) {
+  if (answer==="elegant") {
+    return "Python";
+  }
+  else if (answer==="agree") {
+    return "Python";
+  }
+  else if (answer==="disagree") {
+    return "not Python";
+  }
+  else if (answer==="nope") {
+    return "not Python";
+  }
+}
+
 $(document).ready(function() {
   $("#all-questions").submit(function(event) {
     event.preventDefault();
+    const answer = whitespace($("input:radio[name=whitespace]:checked").val());
+    $("#rec1").text(answer);
     $("#recommendation1").show();
   });
 });
